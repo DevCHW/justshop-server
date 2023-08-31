d99c660a8b003635c55749bf694b712068594f45
DevCHW <ggoma003@naver.com>
2023-08-31 09:58:25 +0900

    [feat] : 프로젝트 생성
    justshop 프로젝트 생성

---
+++ src/main/java/com/justshop/justshop/JustshopApplication.java
@@ -0,0 +1,13 @@
+package com.justshop.justshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justshop/justshop/JustshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justshop.justshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
