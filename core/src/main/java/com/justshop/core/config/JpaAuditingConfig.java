9dc4e4b441ed996a8135d6ffb347b879581eafd3
DevCHW <ggoma003@naver.com>
2023-09-06 19:59:02 +0900

    [style] : 어노테이션 위아래 위치변경

--- core/src/main/java/com/justshop/core/config/JpaAuditingConfig.java
+++ core/src/main/java/com/justshop/core/config/JpaAuditingConfig.java
@@ -3,7 +3,7 @@ package com.justshop.core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @Configuration
+@EnableJpaAuditing
 public class JpaAuditingConfig {
 }
