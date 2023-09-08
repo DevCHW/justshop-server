55a71bce3ce22e179fdcecb5cac192edb0a95d76
DevCHW <ggoma003@naver.com>
2023-09-08 17:10:55 +0900

    [test] : Member unique 키 존재여부 검사 단위테스트 작성

--- core/src/main/java/com/justshop/core/member/domain/repository/MemberRepository.java
+++ core/src/main/java/com/justshop/core/member/domain/repository/MemberRepository.java
@@ -3,5 +3,20 @@ package com.justshop.core.member.domain.repository;
 import com.justshop.core.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByLoginId(String loginId);
+
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByLoginId(String loginId);
+
 }
