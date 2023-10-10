88017fae115a67dc271e323215324451b3ca4cff
DevCHW <ggoma003@naver.com>
2023-10-10 14:50:40 +0900

    [chore] : spring cloud 관련 모듈들 추가, 의존성, 프로퍼티 설정

--- module-app/delivery-service/src/main/java/com/justshop/delivery/DeliveryServiceApplication.java
+++ module-app/delivery-service/src/main/java/com/justshop/delivery/DeliveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.justshop.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.justshop")
+@EnableDiscoveryClient
 public class DeliveryServiceApplication {
 
     public static void main(String[] args) {
