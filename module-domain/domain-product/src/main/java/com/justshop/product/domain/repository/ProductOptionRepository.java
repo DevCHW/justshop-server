aa416d3972c2f9aeb989bdc17b53dd2307e66eb2
DevCHW <ggoma003@naver.com>
2023-10-31 18:28:57 +0900

    [feat] : 상품 옵션 조회 로직에 Race Condition 방지Pessimistic Lock 적용

--- module-domain/domain-product/src/main/java/com/justshop/product/domain/repository/ProductOptionRepository.java
+++ module-domain/domain-product/src/main/java/com/justshop/product/domain/repository/ProductOptionRepository.java
@@ -3,11 +3,16 @@ package com.justshop.product.domain.repository;
 import com.justshop.product.domain.entity.ProductOption;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
     @EntityGraph(attributePaths = {"product"})
     List<ProductOption> findAllByIdIn(List<Long> ids);
 }
